802bc3a1831c00ae0f0039b3bf00d6fece4d363d
djmattyg007 <git@matthewgamble.net>
2014-08-07 21:38:27 +1000

    Add a debug mode to the Brainfuck class

--- bfinterpreter.py
+++ bfinterpreter.py
@@ -28,11 +28,12 @@ class Tape:
         print(chr(self.cells[self.pointer]), end="")
 
 class Brainfuck:
-    def __init__(self, tape, program, allow_nested_loops = True):
+    def __init__(self, tape, program, allow_nested_loops = True, debug = False):
         self.tape = tape
         self.program = program
         self.pointer = 0
         self.allow_nested_loops = allow_nested_loops
+        self.debug = debug
         self.basic_ops = {
             "+" : self.tape.inc_val,
             "-" : self.tape.dec_val,
@@ -54,8 +55,11 @@ class Brainfuck:
                 nested_loop_count += 1
 
     def run_program(self):
+        if self.debug == True:
+            import time
         loop_pointers = []
-        while self.pointer < len(self.program):
+        program_length = len(self.program)
+        while self.pointer < program_length:
             char = self.program[self.pointer]
             if char in self.basic_ops.keys():
                 self.basic_ops[char]()
@@ -75,26 +79,36 @@ class Brainfuck:
                     self.pointer += 1
                 else:
                     self.pointer = loop_start
+            if self.debug == True and self.pointer < program_length:
+                print(self.pointer, "\t", self.program[self.pointer], "\t", self.tape.pointer, "\t", self.tape.get_val())
+                time.sleep(0.1)
 
 
 if __name__ == "__main__":
     import sys
 
+    def parse_bool(string):
+        if string == "true" or string == "y" or string == "yes":
+            return True
+        elif string == "false" or string == "n" or string == "no":
+            return False
+        else:
+            return None
+
     program = ""
     allow_nested_loops = True
+    debug = False
 
     args = sys.argv[1:]
     for x, arg in enumerate(args):
         if arg == "--program":
             program = args[x + 1]
         elif arg == "--nested-loops":
-            if args[x + 1] == "true":
-                allow_nested_loops = True
-            elif args[x + 1] == "false":
-                allow_nested_loops = False
-
+            allow_nested_loops = parse_bool(args[x + 1])
+        elif arg == "--debug":
+            debug = parse_bool(args[x + 1])
 
     tape = Tape()
-    brainfuck = Brainfuck(tape, program, allow_nested_loops)
+    brainfuck = Brainfuck(tape, program, allow_nested_loops, debug)
     brainfuck.run_program()
 
